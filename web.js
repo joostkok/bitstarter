b950c854838e5c588f4de3ff1a5a87af04d74a61
Ubuntu <ubuntu@ip-172-31-18-195.us-west-2.compute.internal>
2013-07-21 11:41:15 +0000

    port update web.js

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var fs = require('fs');
+var express = require("express");
+var fs = require("fs");
 
 var app = express.createServer(express.logger());
 var home = fs.readFileSync('index.html', 'utf-8');
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
   response.send(home);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
